6a88e6a6bf157c4627b6d809ab19d434a9cd61ad
Agustin Morinigo <agustinmorinigo1999@gmail.com>
2025-08-04 12:46:14 -0300

    feat: add signin with google provider

--- src/modules/auth/components/AuthGuard.tsx
+++ src/modules/auth/components/AuthGuard.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from 'react-router';
-import { useIsAuthenticated } from '@/hooks/use-is-authenticated';
+import { useIsAuthenticated } from '@/modules/auth/hooks/use-is-authenticated';
 
 export const AuthGuard = () => {
-    const isAuthenticated = useIsAuthenticated();
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+  const isAuthenticated = useIsAuthenticated();
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
--- src/router.tsx
+++ src/router.tsx
@@ -2,51 +2,51 @@ import { createBrowserRouter, redirect } from 'react-router';
 import { PrivateLayout } from '@/layouts/PrivateLayout';
 import { PublicLayout } from '@/layouts/PublicLayout';
 import { AuthGuard } from '@/modules/auth/components/AuthGuard';
+import { isAuthenticated } from '@/modules/auth/utils/is-authenticated';
 import DashboardPage from '@/pages/Dashboard';
 import LoginPage from '@/pages/Login';
 import NotFoundPage from '@/pages/NotFound';
-import { isAuthenticated } from '@/utils/is-authenticated';
 
 const rootLoader = async () => {
-    if (await isAuthenticated()) {
-        return redirect('/dashboard');
-    } else {
-        return redirect('/login');
-    }
+  if (await isAuthenticated()) {
+    return redirect('/dashboard');
+  } else {
+    return redirect('/login');
+  }
 };
 
 const publicLoader = async () => {
-    if (await isAuthenticated()) {
-        return redirect('/dashboard');
-    }
-    return null;
+  if (await isAuthenticated()) {
+    return redirect('/dashboard');
+  }
+  return null;
 };
 
 const router = createBrowserRouter([
-    {
+  {
+    path: '/',
+    loader: rootLoader,
+  },
+  {
+    path: '/',
+    element: <PublicLayout />,
+    loader: publicLoader,
+    children: [{ path: 'login', element: <LoginPage /> }],
+  },
+  {
+    element: <AuthGuard />,
+    children: [
+      {
         path: '/',
-        loader: rootLoader,
-    },
-    {
-        path: '/',
-        element: <PublicLayout />,
-        loader: publicLoader,
-        children: [{ path: 'login', element: <LoginPage /> }],
-    },
-    {
-        element: <AuthGuard />,
-        children: [
-            {
-                path: '/',
-                element: <PrivateLayout />,
-                children: [{ path: 'dashboard', element: <DashboardPage /> }],
-            },
-        ],
-    },
-    {
-        path: '*',
-        element: <NotFoundPage />,
-    },
+        element: <PrivateLayout />,
+        children: [{ path: 'dashboard', element: <DashboardPage /> }],
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
+  },
 ]);
 
 export default router;
