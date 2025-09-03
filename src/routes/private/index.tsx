b8bdcabdcb9222df206ba72982709364ddefeb51
Agustin Morinigo <agustinmorinigo1999@gmail.com>
2025-09-02 23:10:06 -0300

    feat: add role selector

--- src/routes/private/index.tsx
+++ src/routes/private/index.tsx
@@ -1,9 +1,9 @@
 import { Navigate, type RouteObject } from 'react-router';
+import getDefaultRouteByRole from '@/modules/roles/utils/get-default-route-by-role';
 import ValidationPage from '@/pages/validation';
 import accountingRoutesConfig from '@/routes/private/accounting';
 import attendanceRoutesConfig from '@/routes/private/attendance';
 import userManagementRoutesConfig from '@/routes/private/user-management';
-import getDefaultRouteByRole from '@/utils/get-default-route-by-role';
 
 const privateRoutes: RouteObject[] = [
   accountingRoutesConfig,
